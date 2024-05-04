f0490563cb62c6e6f113c5c76cd3f21b25bb18e5
Carmen Thom <thomrya22@outlook.com>
2024-05-03 22:37:33 -0500

    final model update

--- model.py
+++ model.py
@@ -2,96 +2,112 @@ import os
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader, TensorDataset
+from torch.nn.utils import clip_grad_norm_
 from transformers import BertTokenizer, BertForSequenceClassification
+from torch.optim.lr_scheduler import StepLR
 from torch.optim import AdamW
-from sklearn.metrics import classification_report
 
-def tokenize_data(texts, labels, model_name, max_length=128):
-    tokenizer = BertTokenizer.from_pretrained(model_name, do_lower_case=True)
-    inputs = tokenizer(texts, padding='max_length', truncation=True, max_length=max_length, return_tensors='pt')
-    labels = torch.tensor(labels) 
-    return inputs, labels
-
-def prepare_data(train_df, test_df, val_df, model_name):
-    train_texts, train_labels = train_df['text'].tolist(), train_df['label'].tolist()
-    test_texts, test_labels = test_df['text'].tolist(), test_df['label'].tolist()
-    val_texts, val_labels = val_df['text'].tolist(), val_df['label'].tolist()
-
-    train_inputs, train_labels = tokenize_data(train_texts, train_labels, model_name)
-    test_inputs, test_labels = tokenize_data(test_texts, test_labels, model_name)
-    val_inputs, val_labels = tokenize_data(val_texts, val_labels, model_name)
-
-
-    train_data = TensorDataset(train_inputs['input_ids'], train_inputs['attention_mask'], train_labels)
-    test_data = TensorDataset(test_inputs['input_ids'], test_inputs['attention_mask'], test_labels)
-    val_data = TensorDataset(val_inputs['input_ids'], val_inputs['attention_mask'], val_labels)
-
-    return train_data, test_data, val_data
-
-def train_model(train_data, test_data, val_data, model_name, epochs=5, batch_size=32, learning_rate= 5e-5, num_workers=0, dropout_rate=0.01):
+def train_model(model_name, epochs=5, batch_size=32, max_length = 64, learning_rate= 5e-5, num_workers=0, dropout_rate=0.01):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = BertForSequenceClassification.from_pretrained(model_name, num_labels=1, attention_probs_dropout_prob=dropout_rate, hidden_dropout_prob=dropout_rate).to(device)
-    optimizer = AdamW(model.parameters(), lr=learning_rate)
-    criterion = torch.nn.BCELoss()  
-    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    tokenizer = BertTokenizer.from_pretrained('google/bert_uncased_L-4_H-512_A-8')
+    optimizer = AdamW([
+        {'params': model.bert.parameters(), 'lr': learning_rate},
+        {'params': model.classifier.parameters(), 'lr': learning_rate}
+    ], lr=learning_rate)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    train_df = pd.read_csv('dataset/train.csv')
+    val_df = pd.read_csv('dataset/val.csv')
+
+    train_texts = list(train_df["text"])
+    train_labels = torch.tensor(list(train_df["label"]), dtype=torch.long)
+    val_texts = list(val_df["text"])
+    val_labels = torch.tensor(list(val_df["label"]), dtype=torch.long)
+
+    tokenized_train_texts = tokenizer(train_texts, padding=True, truncation=True, return_tensors="pt", max_length=max_length)
+    tokenized_val_texts = tokenizer(val_texts, padding=True, truncation=True, return_tensors="pt", max_length=max_length)
+
+    train_dataset = TensorDataset(tokenized_train_texts['input_ids'], tokenized_train_texts['attention_mask'], train_labels)
+    val_dataset = TensorDataset(tokenized_val_texts['input_ids'], tokenized_val_texts['attention_mask'], val_labels)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    scheduler = StepLR(optimizer, step_size=1, gamma=0.1)
+
     print("***BEGINNING TRAINING***")
+    train_losses = []
+    val_losses = []
+    val_accuracies = []
+    train_accuracies = []
     for epoch in range(epochs):
         model.train()
+        train_loss = 0.0
+        tr_correct_preds = 0
         for batch in train_loader:
             input_ids, attention_mask, labels = batch
             input_ids, attention_mask, labels = input_ids.to(device), attention_mask.to(device), labels.to(device)
+            
             optimizer.zero_grad()
-            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-            logits = torch.sigmoid(outputs.logits.squeeze(dim=1)) 
-            loss = criterion(logits, labels.float())
-            loss.backward()
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            tr_loss = outputs.loss
+            train_loss += tr_loss.item()
+            tr_loss.backward()
+
+            tr_logits = outputs.logits
+            tr_preds = torch.argmax(tr_logits, dim=1)
+            tr_correct_preds += torch.sum(tr_preds == labels).item()
+            
+            clip_grad_norm_(model.parameters(), max_norm=1.0)
+
             optimizer.step()
+        
+        scheduler.step()
+        
+        # Calculate average training loss for the epoch
+        avg_train_loss = train_loss / len(train_loader)
+        train_losses.append(avg_train_loss)
+        
+        train_accuracy = tr_correct_preds / len(train_df)
+        train_accuracies.append(train_accuracy)
 
         model.eval()
-        val_predictions = []
-        val_true_labels = []
+        val_loss = 0.0
+        correct_preds = 0
+
         with torch.no_grad():
             for batch in val_loader:
                 input_ids, attention_mask, labels = batch
                 input_ids, attention_mask, labels = input_ids.to(device), attention_mask.to(device), labels.to(device)
-                outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-                logits = torch.sigmoid(outputs.logits.squeeze(dim=1))
-                val_predictions.extend(torch.round(torch.sigmoid(logits)).cpu().numpy())
-                val_true_labels.extend(labels.cpu().numpy())
 
-        val_classif = classification_report(val_true_labels, val_predictions)
-        print(f'Epoch {epoch + 1}/{epochs}, Classification Report:\n{val_classif}')
+                outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+                loss = outputs.loss
+                val_loss += loss.item()
 
-        checkpoint = {
-            'epoch': epoch + 1,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'Classification Report (Validation)': val_classif,
-        }
-        if not os.path.exists("saved_models"):
-            os.makedirs("saved_models")
-        torch.save(checkpoint, os.path.join("saved_models", f'checkpoint_epoch_{epoch + 1}.pt'))
+                logits = outputs.logits
+                preds = torch.argmax(logits, dim=1)
+                correct_preds += torch.sum(preds == labels).item()
+                
+        # Calculate average validation loss for the epoch
+        avg_val_loss = val_loss / len(val_loader)
+        val_losses.append(avg_val_loss)        
 
-    # Save final model
-    torch.save(model.state_dict(), os.path.join('saved_models', 'final_model.pt'))
+        val_accuracy = correct_preds / len(val_d)
+        val_accuracies.append(val_accuracy)
+        print(f"Epoch {epoch + 1}/{num_epochs}, Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy:.4f}, Training Loss: {train_loss:.4f}, Training Accuracy: {train_accuracy:.4f}")
+
+    torch.save(model, "saved_model.pt")
 
 def main():
-    MODEL = 'google-bert/bert-base-uncased'
+    MODEL = 'google/bert_uncased_L-4_H-512_A-8'
     EPOCHS = 5 # Default 5
-    BATCH_SIZE = 32 # Default 32
+    BATCH_SIZE = 64 # Default 32
+    MAX_LENGTH = 64 # Default 64
     LEARNING_RATE = 5e-5 # Default 5e-5
     NUM_WORKERS = 0 # Default 0
     DROPOUT = 0.01 # Default 0.01
 
-    train_df = pd.read_csv('dataset/train.csv')
-    test_df = pd.read_csv('dataset/test.csv')
-    val_df = pd.read_csv('dataset/val.csv')
-    print("***DATASETS LOADED***")
-    train_data, test_data, val_data = prepare_data(train_df, test_df, val_df, MODEL)
-    print("***DATA PREPAIRED***")
-    train_model(train_data, test_data, val_data, MODEL, EPOCHS, BATCH_SIZE, LEARNING_RATE, NUM_WORKERS, DROPOUT)
+    train_model(MODEL, EPOCHS, BATCH_SIZE, MAX_LENGTH, LEARNING_RATE, NUM_WORKERS, DROPOUT)
 
 if __name__ == '__main__':
     main()
